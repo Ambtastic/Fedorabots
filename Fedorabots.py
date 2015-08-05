9dec44a82def82cae91ae09d2df74d9fa8a59473
nanibot <yananguitar93@gmail.com>
2015-08-05 10:37:35 -0400

    Code

---
+++ Fedorabots.py
@@ -0,0 +1,47 @@
+from Myro import *
+init(port)
+setName("SEB")
+
+degrees = 0
+left = 0
+right = 0
+degW = .35
+
+def scan():
+    while getObstacle(1) <= 6399: #starts moving with 6400 IR detection
+      forward(1)
+      getObstacle(1) #No obj found
+      #checks for obstacle if none found, continues moving and checking
+    else: #if obj is found
+        stop()
+                
+        while getObstacle(1) >= 6399: #Scans again if osbstacle detected
+            turnBy(5, "deg")#Rotates to scan right
+            getObstacle(1) #Scans
+            degrees += 5 #Keeps track of degs
+            right += 1 #keeps track of how many times it has to turn right to determine which side is longer
+        else:
+            turnBy(- degrees, "deg")
+        while getObstacle(1) >=6399: #Scans again if obstacle detected
+            turnBy(-5, "deg")#Rotates to scan left
+            getObstacle(1) #Scans
+            degrees -= 5 #Keeps track of degs
+            left += 1 #keeps track of how many times it has to turn left to determine which side is longer
+        else:
+            turnBy(- degrees, "deg")#straighten out
+            if right < left:
+                turnBy(90, "deg")#turn right
+                dist = right*degW
+                forward(1, dist + 1)
+                turnBy(-90, "deg")#cont on path
+                scan()#Restarts entire function
+            elif left < right:
+                turnBy(-90, "deg")#turn left
+                dist = left*degW
+                forward(1,dist + 1)
+                turnBy(90, "deg")#cont on path
+                scan()#restart function
+scan()
+
+#beep(<time, frequency>)
+#speak(<something>)
